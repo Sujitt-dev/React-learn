84071d8f74696c7bcd036157052dd08e639fefb3
Sujit kumar <sujittkumar11@gmail.com>
2026-06-20 00:11:42 +0530

    fetch todo from url useing useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
-import ProfileCard from "./ProfileCard";
-import TaskForm from "./TaskForm";
-import TaskList from "./TaskList";
-import {useState} from "react"
+import {useState} from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [Count, setCount]=useState(0);
-  const [skil, setskil]=useState([]);
-  const [Inputs, setInputs]=useState("");
-  
+  const [todos, settodos]=useState([]);
+  const [Loading, setLoading]=useState(true)
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+    .then((res)=>(res.json())).then((data)=>{settodos(data),setLoading(false)})
+  },[])
   return(
     <>
-    <div>
-      <TaskList skil={skil} />
-      <TaskForm Inputs={Inputs} setInputs={setInputs}/>
-      <button onClick={()=>{setskil([...skil,Inputs]),setInputs("")}}>Add Skill</button>
-    </div>
+     <h2>ToDo List</h2>
 
-    <div>
-      <h1>{Count}</h1>
-      <button onClick={()=>setCount(Count+1)}>Increment</button>
-      <button onClick={()=>setCount(Count-1)}>Decrement</button>
-      <button onClick={()=>setCount(0)} >Reset</button>
-    </div>
-    
-    <ProfileCard  name="sujitt" age={25} role={"Developer"}  skills={["java","python","javascript","react","node js","sql"]} bio_info={"highly skiled and good problem solver"} isAvailable={true}/>
-
-    <ProfileCard  name="rahul" age={24} role={"Developer"}  skills={["javascript","react","node js"]} bio_info={"good communictor and best in management"}/>
-
-    <ProfileCard  name="sumit" age={25} role={"Developer"}  skills={["javascript","react","node js","AWS"]} bio_info={"extriemly good at cloud "}/>
-   
-   </>
-
-   
+     {Loading ? <p>Loading....</p>:(
+      <ul>
+        {todos.map((x)=>{
+          return(
+            <li key={x.id}>{x.title}</li>
+          )
+        })}
+      </ul>
+     )}
+    </>
   )
 }
 export default App
