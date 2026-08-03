4e3f8d35c6aa488a2b1cedc19292ea1fc4c2a2b3
Sujit kumar <sujittkumar11@gmail.com>
2026-08-04 02:05:10 +0530

    revision - controlled input with list

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,29 @@ import {useState} from "react";
 import { useEffect } from "react";
 
 function App() {
-  const [todos, settodos]=useState([]);
-  const [Loading, setLoading]=useState(true)
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-    .then((res)=>(res.json())).then((data)=>{settodos(data),setLoading(false)})
-  },[])
-  return(
-    <>
-     <h2>ToDo List</h2>
+const [name,setname]=useState([]);
+const [input, setinput]=useState("");
 
-     {Loading ? <p>Loading....</p>:(
-      <ul>
-        {todos.map((x)=>{
-          return(
-            <li key={x.id}>{x.title}</li>
-          )
-        })}
-      </ul>
-     )}
-    </>
-  )
+return(
+  <>
+  <input type="text"
+  placeholder="enter names" 
+  value={input} 
+  onChange={(e)=>setinput(e.target.value)}/>
+  <br />
+  <button 
+  onClick={(e)=>{
+    setname([...name,input]);
+    setinput("");
+
+  }}
+   >Add name</button>
+
+   <ul>
+    {name.map((e)=>{
+      return <li key={e}>{e}</li>    })}
+   </ul>
+  </>
+)
 }
 export default App
